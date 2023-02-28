8cabf1f1e5bc5c9b4e1e41cc0c043a09427d67f6
Matias Colina <matiasecolina@gmail.com>
2023-02-28 10:21:21 -0300

    PROYECTO FINAL

---
+++ PROYECTO FINAL/cv.js
@@ -0,0 +1,49 @@
+
+function detalle_experiencia() {
+    var vermastexto_exp = document.getElementById("vermastexto_exp");
+    var detalle_boton_exp = document.getElementById ("detalle_boton_exp")
+
+    if (vermastexto_exp.style.display === "none") {
+        vermastexto_exp.style.display = "block";
+        detalle_boton_exp.innerHTML = 'Ver Menos';
+    } else {
+        vermastexto_exp.style.display = "none";
+        detalle_boton_exp.innerHTML = 'Ver Más';
+    }
+  }
+function detalle_estudios() {
+    var vermastexto_est = document.getElementById("vermastexto_est");
+    var detalle_boton_est = document.getElementById ("detalle_boton_est")
+
+    if (vermastexto_est.style.display === "none") {
+        vermastexto_est.style.display = "block";
+        detalle_boton_est.innerHTML = 'Ver Menos';
+    } else {
+        vermastexto_est.style.display = "none";
+        detalle_boton_est.innerHTML = 'Ver Más';
+    }
+  }
+function detalle_idiomas() {
+    var vermastexto_idi = document.getElementById("vermastexto_idi");
+    var detalle_boton_idi = document.getElementById ("detalle_boton_idi")
+
+    if (vermastexto_idi.style.display === "none") {
+        vermastexto_idi.style.display = "block";
+        detalle_boton_idi.innerHTML = 'Ver Menos';
+    } else {
+        vermastexto_idi.style.display = "none";
+        detalle_boton_idi.innerHTML = 'Ver Más';
+    }
+  }
+function detalle_referencia() {
+    var vermastexto_ref = document.getElementById("vermastexto_ref");
+    var detalle_boton_ref = document.getElementById ("detalle_boton_ref")
+
+    if (vermastexto_ref.style.display === "none") {
+        vermastexto_ref.style.display = "block";
+        detalle_boton_ref.innerHTML = 'Ver Menos';
+    } else {
+        vermastexto_ref.style.display = "none";
+        detalle_boton_ref.innerHTML = 'Ver Más';
+    }
+  }
