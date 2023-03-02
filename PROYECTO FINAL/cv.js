21cfe937de8765b12b9c730aa11bc46873574cae
MColina1705 <124734387+MColina1705@users.noreply.github.com>
2023-03-02 14:07:25 -0300

    Update cv.js

--- PROYECTO FINAL/cv.js
+++ PROYECTO FINAL/cv.js
@@ -3,11 +3,11 @@ function detalle_experiencia() {
     var vermastexto_exp = document.getElementById("vermastexto_exp");
     var detalle_boton_exp = document.getElementById ("detalle_boton_exp")
 
-    if (vermastexto_exp.style.display === "none") {
+    if (vermastexto_exp.style.display === "") {
         vermastexto_exp.style.display = "block";
         detalle_boton_exp.innerHTML = 'Ver Menos';
     } else {
-        vermastexto_exp.style.display = "none";
+        vermastexto_exp.style.display = "";
         detalle_boton_exp.innerHTML = 'Ver Más';
     }
   }
@@ -15,11 +15,11 @@ function detalle_estudios() {
     var vermastexto_est = document.getElementById("vermastexto_est");
     var detalle_boton_est = document.getElementById ("detalle_boton_est")
 
-    if (vermastexto_est.style.display === "none") {
+    if (vermastexto_est.style.display === "") {
         vermastexto_est.style.display = "block";
         detalle_boton_est.innerHTML = 'Ver Menos';
     } else {
-        vermastexto_est.style.display = "none";
+        vermastexto_est.style.display = "";
         detalle_boton_est.innerHTML = 'Ver Más';
     }
   }
@@ -27,11 +27,11 @@ function detalle_idiomas() {
     var vermastexto_idi = document.getElementById("vermastexto_idi");
     var detalle_boton_idi = document.getElementById ("detalle_boton_idi")
 
-    if (vermastexto_idi.style.display === "none") {
+    if (vermastexto_idi.style.display === "") {
         vermastexto_idi.style.display = "block";
         detalle_boton_idi.innerHTML = 'Ver Menos';
     } else {
-        vermastexto_idi.style.display = "none";
+        vermastexto_idi.style.display = "";
         detalle_boton_idi.innerHTML = 'Ver Más';
     }
   }
@@ -39,11 +39,11 @@ function detalle_referencia() {
     var vermastexto_ref = document.getElementById("vermastexto_ref");
     var detalle_boton_ref = document.getElementById ("detalle_boton_ref")
 
-    if (vermastexto_ref.style.display === "none") {
+    if (vermastexto_ref.style.display === "") {
         vermastexto_ref.style.display = "block";
         detalle_boton_ref.innerHTML = 'Ver Menos';
     } else {
-        vermastexto_ref.style.display = "none";
+        vermastexto_ref.style.display = "";
         detalle_boton_ref.innerHTML = 'Ver Más';
     }
   }
